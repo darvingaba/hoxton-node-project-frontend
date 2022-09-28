77023fe633769c1c50af0cbf75f625b954fa922a
Aris Alshiqi <arisalshiqi111@gmail.com>
2022-09-28 15:58:40 +0200

    Nft Details

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes,Route } from 'react-router-dom'
 import './App.css'
 import { Header } from './components/Header'
 import { Home } from './pages/Home'
+import { NftDetails } from './pages/NftDetails'
 import { SignIn } from './pages/SignIn'
 import { SignUp } from './pages/SignUp'
 
@@ -48,6 +49,7 @@ function App() {
           element={<SignIn user={user} setUser={setUser} />}
         />
         <Route path="/home" element={<Home />} />
+        <Route path="/nfts/:id" element={ <NftDetails /> } />
       </Routes>
 
     </div>
